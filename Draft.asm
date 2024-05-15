caa5058174419ed00980bd7a2c48859a822681e8
byte-engineer <blalalasmayly1998@gmail.com>
2024-05-15 21:17:55 +0400

    Asm draft
    i make it more readable

--- Draft.asm
+++ Draft.asm
@@ -1,51 +1,49 @@
-;ESP --> the pionter
-;EIP --> the instruction pionter
-;pop --> cut the value that indicated by the pointer to a certain register
-;push -> to push a value on the stack (the pointer will be point in it)
-;call -> call functions it pushs the first instruction to the stack then it continue the other instruction
-;ret --> it returns to main function also is pops next instruction into EIP 
+;------------------------------------------------------------------------------------------------
+;|> This draft is for learn asembly x86 NASM sytax.
+;|> We have to run the code in lunix machine.
+;|> If we want to run it in windows we can use (WSL) feature which is built in the system.
+;------------------------------------------------------------------------------------------------
+;Commands to run the code:
+;-sudo apt-get install nasm                   |Download NASM
+;-nasm -f elf32 input.asm -o output.o         |convert file from .asm --> .o
+;-ld -m elf_i386 input.o -o output            |convert converted file from .o --> .exe //this the linker.
+; nasm -f elf32 -g file.asm                   ;for x32 architecture.
+; ld -m alf_i3x86 -o file file.o              ;for x32 architecture.
+;-./output                                    |run the .exe file
+;------------------------------------------------------------------------------------------------
 
-
-;----------------------------------------------------------------------------------------------
 
 section .date                        ;To define a constant variables.
 
-;could be strings, numbers, termonating strings...
+    var-name dd 10                   ;Declare and initialize a 32-bit integer variable
 
 section .bss                         ;To reserve a space in memory to store data later
 
+section .text                        ; executable code section.
 _start:                              ; from here the code start executing.
-main:                                ; main function.
 
-; nasm -f elf64 -g file.asm          ; it outputs file.o
-; ld -m alf_x86_64 -o file file.o    ; use dynamic linker to compile the code.(it start executing from _start lable)
-; gcc -o output input.s              ; use GCC to compile the code.
-
-; nasm -f elf32 -g file.asm          ; for x32 architecture
-; ld -m alf_i3x86 -o file file.o     ; for x32 architecture
-
-; registers : Hardware emplemetation of variables.
-
-int a = 1                            ; define an integer variable.
-char[] b = [0, 0, 0]                 ; asign "000" to avariable as a list to create a string. 
+main:                                ; main function.(not required)
+ 
 
 ;---------------------------------------------------------------------------------------------
 
+; registers : Hardware emplemetation of variables.
 ; eax  ==> 32 bit register           ;
 ; rax  ==> 64 bit register           ;
 
 ; If we want to get data from memory we put the adderss of data on square brakets.
 ;                                    |> We can store data or addresses in registers.
 
+;This are some regesters
 ;EAX  --> 32 bit  | EAH, EAL 8 bits  |> X prefix for whole register.
 ;EBX  --> 32 bit  | EBH, EBL 8 bits  |> H prefix for third quarter in the regester.
 ;ECX  --> 32 bit  | ECH, ECL 8 bits  |> L prefix for last quarter in the regester.
-;EDX  --> 32 bit  | EDH, EDL 8 bits  |
+;EDX  --> 32 bit  | EDH, EDL 8 bits
 ;ESI  --> source index
 ;EDI  --> destination index
 ;ESP  --> stack pointer              |> It points in top of the stack.
 ;EBP  --> base pointer               |> It points in the base of the stack.
-;                                    |> The stack is empty when ESP and EBP has a same address
+;                                    |> The stack is empty when ESP and EBP has a same address.
 
 ;MOV-------------------------------------------------------------------------------------------------------
 
@@ -63,5 +61,40 @@ movsx edx, byte ptr [memory adderss];|> Copies the byte from the memory location
 mov   al , -5           ; AL  = -5                            (11111011) 
 movsx eax, al           ; EAX = -5 (11111111 11111111 11111111 11111011) sign-extended to 32 bits.
 
-;-------------------------------------------------------------------------------------------------------
+;LOGICAL OPERATIONS-----------------------------------------------------------------------------------------
+
+;These operations include AND, OR, XOR, and NOT
+
+;                     ; AND operation for each singel bit
+mov eax, 0x0F0F0F0F   ;                                                   |0x0F0F0F0F = 00001111 00001111 00001111 00001111
+and eax, 0x33333333   ;       00001111 00001111 00001111 00001111 AND     |0x33333333 = 00110011 00110011 00110011 00110011
+;                     ;       00110011 00110011 00110011 00110011
+;The result stored in EAX   = 00000011 00000011 00000011 00000011
+
+
+;                     ; OR operation for each singel bit
+mov eax, 0x0F0F0F0F   ;
+or  eax, 0x33333333   ;       00001111 00001111 00001111 00001111 OR
+;                     ;       00110011 00110011 00110011 00110011
+;The result stored in EAX   = 00111111 00111111 00111111 00111111
+
+
+mov eax, 0x0F0F0F0F   ; XOR operation for each singel bit
+xor eax, 0x33333333   ; EAX = 00001111 00001111 00001111 00001111 XOR
+;                     ;       00110011 00110011 00110011 00110011
+;The result stored in EAX   = 00111100 00111100 00111100 00111100
+
+
+mov eax, 0x0F0F0F0F   ; EAX = 00001111 00001111 00001111 00001111        |NOT operation for each singel bit
+not eax               ; EAX = 11110000 11110000 11110000 11110000
+
+
+mov  eax, 0x0F0F0F0F  ;
+test eax, 0x33333333  ; Perform bitwise AND
+;                     ;         00001111 00001111 00001111 00001111 AND
+;                     ;         00110011 00110011 00110011 00110011
+;                     ; Result: 00000011 00000011 00000011 00000011
+
+
+;LOGICAL OPERATIONS-----------------------------------------------------------------------------------------
 
