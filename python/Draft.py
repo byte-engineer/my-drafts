b99c90a5804a53af5038ad9a1613e7569a8482a2
byte-engineer <blalalasmayly1998@gmail.com>
2024-05-23 22:36:49 +0400

    Update Draft.py

--- python/Draft.py
+++ python/Draft.py
@@ -1,15 +1,14 @@
 
-#############     #####         #####   #########################  ######             ######          ###########           #########        ######
-###############    #####       #####    #########################  ######             ######       ##################       ##########       ######
-#####      #####    #####     #####     #########################  ######             ######   ########         ########    ###########      ######
-#####      #####      ##### #####                ######            ######             ######  #######             #######   ###### ######    ######
-#####     #####         #######                  ######            #########################  #######              #######  ######  ######   ######
-#############            #####                   ######            #########################  #######              #######  ######   ######  ######
-#####                    #####                   ######            ######             ######   #######            #######   ######     ##### ######
-#####                    #####                   ######            ######             ######    ########        ########    ######      ###########
-#####                    #####                   ######            ######             ######       ##################       ######       ##########
-#####                    #####                   ######            ######             ######          ###########           ######        #########
-
+#############     #####         #####  #######################  ######          ######          ###########           #########        ######
+###############    #####       #####   #######################  ######          ######       ##################       ##########       ######
+#####      #####    #####     #####    #######################  ######          ######   ########         ########    ###########      ######
+#####      #####      ##### #####              ######           ######          ######  #######             #######   ###### ######    ######
+#####     #####         #######                ######           ######################  #######              #######  ######  ######   ######
+#############            #####                 ######           ######################  #######              #######  ######   ######  ######
+#####                    #####                 ######           ######          ######   #######            #######   ######     ##### ######
+#####                    #####                 ######           ######          ######    ########        ########    ######      ###########
+#####                    #####                 ######           ######          ######       ##################       ######       ##########
+#####                    #####                 ######           ######          ######          ###########           ######        #########
 
 #INTRO-----------------------------------------------------------------
 
@@ -50,7 +49,7 @@ print("Force it", flush= True)                                         # To forc
 # print("Hi guys! ")                                                   # We can use comment to prevent code form running.
 """This is not commend"""                                              # This is non-assigned string(text).
 
-#DATATYPES-------------------------------------------------------------
+#DATA TYPES------------------------------------------------------------
 
 #|> We use built-in function called type() to detrim the type of data.
 #|> Data types in python [ints, floats, strings, lists, tuples, sets, dictionarys]
@@ -88,14 +87,144 @@ help("keywords")                                                       # We use 
 print("this is \nnew line")                                            # Will print "new line" in new line.
 
 # Backslash                                                            # Generaly backslash escape any element after it.
-print("there is great language called \"Python\"")                     # Use backslash it escape double quotes and single qoutes.
+print("there is great language called \"Python\"")                     # Use backslash it escape double quotes and single qoutes.Result => "there is great language called "Python""
 
 # Backspace
 print("Hello,\b World!")                                               # Perform backspace so it delete the comma |Result => "Hello World!"
 
 # tab
-print("This is\ttap")                                                  # tap by defult equals One spaces.         |Result => "This is tap"
+print("This is\ttap")                                                  # tap by defult equals several spaces.         |Result => "This is tap"
 
 # Hex characters
 print("\x48\x65\x6C\x6C\x6F")                                          # We write Hexadecimal value for ASCII character.
 
+#STRINGS---------------------------------------------------------------
+
+text1 = "Hi "
+text2 = "my friend."
+essay = \
+"""
+Hi my ferinds, 
+today I will program in python
+thank you.
+"""                                                                    # We can perform multiple line string by using three double or single quots.
+
+# Concatenation
+full_text = text1 + text2                                              # We use + to merge two strings.
+
+#INDEXING AND SLICING--------------------------------------------------
+
+#|> Our strings are ordered by indexs.
+#|> Python use (Zero Base Indexing) that is mean the first character has index zero.
+#|> Indexing allow for us to accesse any character of string.
+#|> Slicing allow us to accesse many characters of string at once.
+
+Wisdom1 = "Don't fight someone who has nothing to lose"
+
+# Indexing (Access singel character)
+Wisdom1[0]                                                               # Index[0]   => "D"  , We start count from zero.
+Wisdom1[9]                                                               # Index[9]   => "h"
+
+Wisdom1[-1]                                                              # Index[-1]  => "s"  , For negatives we start counting from the last.
+Wisdom1[-37]                                                             # Index[-37] => "f"
+
+# Slicing (Access multiple characters)                                   # Syntax --> srting[start:end:steps] end not included.
+Wisdom1[5: 11]                                                           # Result => "fight"
+Wisdom1[-15: -8]                                                         # If we write a negetive values it will start counting from the last. |Result => "nothing"
+
+Wisdom1[: 11]                                                            # If start not written it will start from zero.                       |Result => "Don't fight"
+Wisdom1[20:]                                                             # If end not written it will go the all the way to the end.           |Result => "who has nothing to lose"
+Wisdom1[:]                                                               # Full string.
+
+Wisdom1[::2]                                                             # Third value is for steps so in this case it will ignore first character and include the second and all the way to the end.
+Wisdom1[::3]                                                             # It will ignore first two characters and include the third and complete all the way to the end.
+
+#STRING METHODS---------------------------------------------------------
+
+Wisdom2 = "  life has a large spaces..!  "
+Counter = "1,2,3,4,5,6,7,8"
+
+len("Learn python")                                                     # It measures the length of the string.     |Result => 12
+
+# strip() rstrip() lstrip()
+Wisdom2.strip()                                                         # Will remove spaces from both sides.
+Wisdom2.rstrip()                                                        # Will remove spaces from right side.
+Wisdom2.lstrip()                                                        # Will remove spaces from lift side.
+
+Wisdom2.split("!.")                                                     # Will remove all "." and "!" from first and end of the string.
+
+# zfill()                                                               # Filling string by adding zeros on the lift
+"10".zfill(3)                                                           # Result => "010"
+
+# title()                                                               # Make the first letter on every word in apper case.
+Wisdom2.title()                                                         # Result => "Life Has A Large Spaces..!  "
+
+# capitalize()                                                          # It just make the first character in apper case.
+Wisdom2.capitalize()                                                    # Result => "Life has a large spaces..!"
+
+# apper() lower()                                                       # Convert all the characters in upper case.
+Wisdom2.upper()                                                         # Result => "  LIFE HAS A LARGE SPACES..!"
+"DONT SMOKE!".lower()                                                   # Result => "dont smoke!"
+
+# split() rsplit()                                                      # Split the string according to certian seprator and but it on a list, syntax => str.split(seprator, number splits).
+Wisdom2.split()                                                         # By defult the seprator set to space " "|Result => ['life', 'has', 'a', 'large', 'spaces..!']
+Counter.split(",")                                                      # Result => ['1', '2', '3', '4', '5', '6', '7', '8'], Set seprator to ",".
+Counter.split(",", 3)                                                   # Result => ['1', '2', '3,4,5,6,7,8'], We have two splits.
+Counter.rsplit(",", 3)                                                  # It start from the right instead of lift |Result => ['1,2,3,4,5', '6', '7', '8']
+
+# center() rjust() ljust()                                              # Insert characters after and before the string, Syntax => str.center(final string length, "filler")
+"Delete".center(10)                                                     # Result => "  Delete  "
+"Dont Delete".center(15, "^")                                           # Result => "^^Dont Delete^^"
+"Delete".rjust(10, "-")                                                 # Result => "Delete----"  
+"Delete".ljust(10, "-")                                                 # Result => "----Delete"
+
+
+# count()                                                               # count number of repetitions, syntax => str.count("target", start, end) --> int
+Wisdom2.count("a")                                                      # Result => 4
+
+# swipcase()                                                            # swips the case of string.
+"bE CAREFUL".swapcase()                                                 # Result => "Be careful"
+
+# startswith() endswith()                                               # Cheak if the string start/end with certian character, syntax => str.startwith("target", start, end)
+"Nice".startswith("N")                                                  # Result => True
+"God game".endswith("e")                                                # Result => True
+
+# index() find()                                                        # find the index for certian character, index("substring", start, end)
+Wisdom2.index("l")                                                      # Result => 2  |if there is no result we will get error.
+Wisdom2.find("o")                                                       # Result => -1 |if there is no result we will get -1.
+
+# expandtaps()                                                          # Control tap size.
+"Python\trespect\ttaps".expandtabs(3)                                   # Every tap will equal 3 spaces.
+
+# islower() isupper()                                                   # Cheak if the string upper or lower
+"HELLO".isupper()                                                       # Result => True
+"hello".islower()                                                       # Result => True
+
+# isnumeric() isalpha() isalnum()                                       # Cheak if string number/lphabtic/both.
+"13".isnumeric()                                                        # Result => True 
+"13".isalpha()                                                          # Result => True 
+"13".isalnum()                                                          # Result => True 
+
+# replace()                                                             # from it's name it replace string with other string
+"one two three one two three".replace("one", "1")                       # Result => "1 two three 1 two three"
+"one two three one two three".replace("one", "1", 1)                    # It will replace first "one" only
+
+# join()                                                                # Convert list to string.
+"-".join(["my", "name", "is", "..."])                                   # Result => "my-name-is-..."
+
+#STRING FORMATTING------------------------------------------------------
+
+OP_1 = "Science"
+OP_2 = "Money"
+year = 1980
+ratio = 0.75
+
+
+# place holders method                                                  # This method also used in C.
+#|> This method use different holder place for different data types:
+#|> %s --> String
+#|> %d --> Integere
+#|> %f --> Float
+
+print("%s and %s unlock any door" % (OP_1, OP_2))                       # Result => "Science and Money unlock any door"
+print("I born in %d,Iam younger than %f poeple!"%(year, ratio))         # Result => "I born in 1980,Iam younger than 0.750000 poeple!"
